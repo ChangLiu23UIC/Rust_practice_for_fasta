eb52e5bee0b07cfada67baad13e9b3d3a7162413
Chang Liu <cliu239@uic.edu>
2023-11-08 12:36:14 -0600

    File read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() -> std::io::Result<()> {
+    let fin = File::open("psm.tsv")?;
+    let mut reader = BufReader::new(fin);
+
+    let mut line = String::new();
+    let len = reader.read_line(&mut line)?;
+    println!("{len} bytes long");
+    Ok(())
 }
